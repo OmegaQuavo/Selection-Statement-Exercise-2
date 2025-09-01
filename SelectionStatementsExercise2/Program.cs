9776536012e68ccf198a0de35dafaec0eb8b56ee
Caleb <cedinsmore63@gmail.com>
2025-09-01 16:58:01 -0400

    completed project

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
         }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I dont prefer math personally.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules!");
+                    break;
+                case "pe":
+                case "phys ed":
+                case "physical education":
+                    Console.WriteLine("Physical education rules!!");
+                    break;
+                case "history":
+                    Console.WriteLine("I too love history");
+                    break;
+                case "geography":
+                    Console.WriteLine("Geography rules!");
+                    break;
+                default:
+                    Console.WriteLine("I like that too.");
+                    break;
+            }
+            
+        }
+        
+        
+        
     }
 }
